a537ac59d020ea2cdb3ddca954c83bfec0b27424
Ayazbek Aibek <aayazbek1936@gmail.com>
2023-09-27 14:40:04 +0600

    third commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import React from "react";
+import "./scss/app.scss";
+import Header from "./components/Header";
+import Categories from "./components/Categories";
+import Sort from "./components/Sort";
+import PizzaBlock from "./components/PizzaBlock";
+
 function App() {
   return (
-    <div className="App">
-     <p>Hello World 123</p>
+    <div class="wrapper">
+      <Header />
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class="content__title">Все пиццы</h2>
+          <div class="content__items">
+            <PizzaBlock title = 'Мексиканская' price = {400} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
