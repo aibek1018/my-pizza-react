fa13f3e697053ab7a160db424c533bbc5af9bac6
Ayazbek Aibek <aayazbek1936@gmail.com>
2023-10-15 23:02:44 +0600

    seventh commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./scss/app.scss";
 import Header from "./components/Header";
 import Home from "./pages/Home";
@@ -6,27 +6,32 @@ import NotFound from "./pages/NotFound";
 import { Routes, Route } from "react-router-dom";
 import Cart from "./pages/Cart";
 
+export const SearchContext = React.createContext("");
+
 function App() {
+  const [searchValue, setSearchValue] = useState("");
+
   return (
     <div className="wrapper">
-      <Header />
-      <div className="content">
+      <SearchContext.Provider value={{ searchValue, setSearchValue }}>
+        <Header />
+        <div className="content">
           <Routes>
             <Route
               path="/"
               element={<Home />}
             />
-              <Route
+            <Route
               path="/cart"
               element={<Cart />}
             />
-             <Route
+            <Route
               path="*"
               element={<NotFound />}
             />
           </Routes>
-       
-      </div>
+        </div>
+      </SearchContext.Provider>
     </div>
   );
 }
--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
-function Categories() {
-  const [activeIndex, setActiveIndex] = React.useState(0);
+function Categories({value, onChangeCategory}) {
 
   const categories = [
     "Все",
@@ -12,20 +11,17 @@ function Categories() {
     "Закрытые",
   ];
 
-  const onClickCategory = (index) => {
-    setActiveIndex(index);
-  };
 
   return (
     <div className="categories">
       <ul>
-        {categories.map((value, index) => (
+        {categories.map((categoryName, index) => (
           <li
             key={index}
-            onClick={() => onClickCategory(index)}
-            className={activeIndex === index ? "active" : ""}
+            onClick={() => onChangeCategory(index)}
+            className={value === index ? "active" : ""}
           >
-            {value}
+            {categoryName}
           </li>
         ))}
       </ul>
